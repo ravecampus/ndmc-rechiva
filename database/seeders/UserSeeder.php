3de44c2e4227ec4f0b817a4e01bd42811c8bae27
Lavwin C. Campollo <rave_campus@hotmail.com>
2024-09-21 14:48:26 +0800

    admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,9 +17,9 @@ class UserSeeder extends Seeder
         DB::table('users')->delete();
         DB::table('users')->insert([
             'id' => 1,
-            'first_name' => 'Raven',
-            'middle_initial' => 'C',
-            'last_name' => 'Campus',
+            'first_name' => 'Admin',
+            'middle_initial' => '',
+            'last_name' => 'Administrator',
             'email' => 'admin@gmail.com',
             'role' => 2,
             'activate' => 1,
