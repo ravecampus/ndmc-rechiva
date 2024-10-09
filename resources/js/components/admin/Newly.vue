469f405aac2e8b74393dc1f517e5875a69676715
Lavwin C. Campollo <rave_campus@hotmail.com>
2024-10-09 22:04:25 +0800

    prof

--- resources/js/components/admin/Newly.vue
+++ resources/js/components/admin/Newly.vue
@@ -76,7 +76,7 @@
     <div class="row">
         <div class="col-md-3 border-right">
             <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                <img class="rounded-circle" width="150px" :src="'/img/user.png'">
+                <img class="rounded-circle" width="150px" :src="user.image == null ? '/img/user.png' : 'data:'+user.extension+';base64,'+user.image">
                 <span class="font-weight-bold fw-bold">
                     {{ user.first_name }}
                     {{ user.middle_initial }}.
